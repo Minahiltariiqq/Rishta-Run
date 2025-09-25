30c4d061313b6706991b07e26b5fa5f5c0582a0d
Minahiltariiqq <minahiltariiqq@gmail.com>
2025-09-25 19:24:53 +0500

    pause menu
    Added a pause menu

---
+++ Assets/clothes.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class clothes : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject playerrr;
+    void Start()
+    {
+        
+    }
+
+    public void enableshirt ()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
