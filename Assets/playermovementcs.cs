d2cb6d77e77db78a19a155a7e37db81d56fa9e55
Minahiltariiqq <minahiltariiqq@gmail.com>
2025-09-26 11:56:29 +0500

    main menu

--- Assets/playermovementcs.cs
+++ Assets/playermovementcs.cs
@@ -1,27 +1,56 @@
 
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
+//public class playermovementcs : MonoBehaviour
+//{
+//    Vector2 moveVector;
+//    public float moveSpeed = (6);
+
+
+
+//    public void InputPlayer(InputAction.CallbackContext _context)
+//    {
+
+//        moveVector = _context.ReadValue<Vector2>();
+//    }
+
+
+//    private void Update()
+//    {
+//        Vector3 movement = new Vector3(moveVector.x, 0, moveVector.y);
+//        movement.Normalize();
+//        transform.Translate(moveSpeed * movement * Time.deltaTime);
+
+//    }
+//}
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class playermovementcs : MonoBehaviour
+
+public class PlayerMovements : MonoBehaviour
 {
-    Vector2 moveVector;
-    public float moveSpeed = (6);
+    public float moveSpeed = 6f;
+    private Vector2 moveVector;
+    private Rigidbody rb;
 
-
-
-    public void InputPlayer(InputAction.CallbackContext _context)
+    void Awake()
     {
-
-        moveVector = _context.ReadValue<Vector2>();
+        rb = GetComponent<Rigidbody>();  // Get Rigidbody attached to Player
     }
 
+    public void InputPlayer(InputAction.CallbackContext context)
+    {
+        moveVector = context.ReadValue<Vector2>();
+    }
 
-    private void Update()
+    void FixedUpdate()
     {
         Vector3 movement = new Vector3(moveVector.x, 0, moveVector.y);
-        movement.Normalize();
-        transform.Translate(moveSpeed * movement * Time.deltaTime);
-
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
